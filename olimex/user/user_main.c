c458bd82ae609a70eeea175c8ce749588d0351b8
TS\bozicekm <miha.bozicek@telekom.si>
2016-10-25 22:24:50 +0200

    Patch 1.5.4.1

--- olimex/user/user_main.c
+++ olimex/user/user_main.c
@@ -30,6 +30,9 @@
 #include "driver/key.h"
 #include "driver/uart.h"
 
+#include "user_flash.h"
+#include "user_preferences.h"
+
 #include "user_button.h"
 #include "user_relay.h"
 #include "user_adc.h"
@@ -40,6 +43,7 @@
 #include "user_switch2.h"
 #include "user_badge.h"
 #include "user_dimmer.h"
+#include "user_robko.h"
 
 #include "user_events.h"
 #include "user_webserver.h"
@@ -65,7 +69,52 @@ void ICACHE_FLASH_ATTR user_rf_pre_init(void) {
 
 void ICACHE_FLASH_ATTR user_init_done() {
 	debug("\nINIT: Done\n\n");
+	/* MihaB added */
 	mb_main();
+	/* End MihaB Added */
+}
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void) {
+	enum flash_size_map size_map = system_get_flash_size_map();
+	uint32 rf_cal_sec = 0;
+	
+	switch (size_map) {
+		case FLASH_SIZE_4M_MAP_256_256:
+			rf_cal_sec = 128 - 5;
+		break;
+		
+		case FLASH_SIZE_8M_MAP_512_512:
+			rf_cal_sec = 256 - 5;
+		break;
+		
+		case FLASH_SIZE_16M_MAP_512_512:
+		case FLASH_SIZE_16M_MAP_1024_1024:
+			rf_cal_sec = 512 - 5;
+		break;
+		
+		case FLASH_SIZE_32M_MAP_512_512:
+		case FLASH_SIZE_32M_MAP_1024_1024:
+			rf_cal_sec = 1024 - 5;
+		break;
+		
+		default:
+			rf_cal_sec = 0;
+		break;
+	}
+	
+	return rf_cal_sec;
 }
 
 /******************************************************************************
@@ -88,7 +137,20 @@ void ICACHE_FLASH_ATTR user_init(void) {
 #if UART1_ENABLE
 	stdout_init(UART1);
 #endif
-
+	
+	flash_region_register("boot.bin",     0x000, 0x001);
+	flash_region_register("user1.bin",    0x001, 0x07B);
+	flash_region_register("user2.bin",    0x081, 0x07B);
+	flash_region_register("user-config",  0x100, 0x001);
+	flash_region_register("PrivateKey",   0x101, 0x001);
+	flash_region_register("Certificate",  0x102, 0x001);
+	/* MihaB changed; I have there settings */
+	// flash_region_register("Preferences",  0x103, 0x001);
+	flash_region_register("Preferences",  0x104, 0x001);
+	/* End MihaB Change */
+	
+	preferences_init();
+	
 	// UART Devices
 #if DEVICE == PLUG
 	user_plug_init();
@@ -158,6 +220,9 @@ void ICACHE_FLASH_ATTR user_init(void) {
 #endif
 #if DEVICE == DIMMER
 	user_dimmer_init();
+#endif
+#if DEVICE == ROBKO
+	user_robko_init();
 #endif
 #endif
 	
